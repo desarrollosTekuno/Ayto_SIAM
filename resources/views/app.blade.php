b25e4288ed1ac562059d5de59bb7750f9a13468e
OsirisHP <Usir95.hp@gmail.com>
2026-01-12 16:38:20 -0600

    icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'SIAM') }}</title>
+        <link rel="icon" href="{{ asset('img/Icon.png') }}">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&family=Patrick+Hand&family=Caveat:wght@400;500;600;700&family=Open+Sans:wght@300;400;500;600;700&family=Nunito:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 
         <!-- Scripts -->
         @routes
