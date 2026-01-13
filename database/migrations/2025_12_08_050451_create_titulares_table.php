9bb9acfb160cd407adc3cee1fa8f65ceaa69b590
OsirisHP <Usir95.hp@gmail.com>
2026-01-12 18:01:12 -0600

    campos para titulares

--- database/migrations/2025_12_08_050451_create_titulares_table.php
+++ database/migrations/2025_12_08_050451_create_titulares_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('titulares', function (Blueprint $table) {
             $table->id();
             $table->string('nombre', 150);
+            $table->string('apellido_paterno', 150)->nullable();
+            $table->string('apellido_materno', 150)->nullable();
+            $table->string('telefono', 20)->nullable();
+            $table->string('extension', 10)->nullable();
+            $table->string('celular', 20)->nullable();
 
             $table->unsignedBigInteger('cargo_id')->nullable();
             $table->foreign('cargo_id')->references("id")->on("cargos");
