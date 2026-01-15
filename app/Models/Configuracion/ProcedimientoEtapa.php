61ae8378abc17e5a9f61873b3c295b2b28bd5b52
Osiris <Usir95.hp@gmail.com>
2026-01-14 23:40:52 -0600

    Nuevo modelo para etapas de procedimientos

---
+++ app/Models/Configuracion/ProcedimientoEtapa.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Configuracion;
+
+use App\Models\Catalogos\Etapa;
+use App\Models\Catalogos\TipoProcedimiento;
+use App\Models\Traits\HasDataTable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProcedimientoEtapa extends Model {
+    use HasFactory, SoftDeletes, HasDataTable;
+
+    protected $table = 'procedimiento_etapas';
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function TipoProcedimiento() {
+        return $this->belongsTo(TipoProcedimiento::class, 'tipo_procedimiento_id');
+    }
+
+    public function Etapa() {
+        return $this->belongsTo(Etapa::class, 'etapa_id');
+    }
+}
