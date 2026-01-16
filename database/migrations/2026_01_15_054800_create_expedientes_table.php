1269356048810a3de2e59d9382644e65239b40b9
Osiris <Usir95.hp@gmail.com>
2026-01-16 14:49:46 -0600

    Reacomodo de catalogos 1

--- database/migrations/2026_01_15_054800_create_expedientes_table.php
+++ database/migrations/2026_01_15_054800_create_expedientes_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
- Schema::create('expedientes', function (Blueprint $table) {
+        Schema::create('expedientes', function (Blueprint $table) {
             $table->id();
 
             $table->string('folio', 35)->unique();
